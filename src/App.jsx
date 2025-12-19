876bf95e371a56b21e28c8c8e1b9027ae0b46da2
srivani <srivanisreedhar05@gmail.com>
2025-12-19 14:32:30 +0530

    Added Counter Component for Q2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import UserData from "./UserData";
+import Counter from "./Counter";
+function App() {
+  return (
+    <div>
+      <UserData />
+      <hr />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Counter.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count !== 0 && count % 3 === 0) {
+      alert(`The current number ${count} is divisible by 3`);
+    }
+  }, [count]); // 👈 runs ONLY when count changes
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
